59be96a7376239235329adfb6de459e66fc2df92
Student <Student@LabVM2047644.5ky0irqfkqgebhftalxgpazyba.jnbx.internal.cloudapp.net>
2026-04-13 15:43:32 +0200

    created a new project

---
+++ src/main/java/com/mycompany/quickchat_1/QuickChat_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quickchat_1;
+
+/**
+ *
+ * @author Student
+ */
+public class QuickChat_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
